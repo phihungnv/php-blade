b9e23eb0df9b31a46010bf6a0c2c687655e9fd47
Hung Nguyen <hung@quape.com>
2021-07-22 20:20:25 +0700

    fix .gitignore

--- tests/cache/6fbd208ed0f0dec0bf7e4a168e767d185534ddbb.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title><?php echo $__env->yieldContent('title'); ?></title>
-    </head>
-    <body>
-        <?php echo $__env->yieldContent('content'); ?>
-    </body>
-</html>
